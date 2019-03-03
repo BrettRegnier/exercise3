96f6309ed78dfb4464bbe155aebfc1952225ee42
Brett Regnier <bretternestregnier@gmail.com>
2019-03-03 09:52:51 -0700

    rotation and geometry creation

--- js/myWebGL.js
+++ js/myWebGL.js
@@ -1,39 +1,125 @@
-window.onload = Init();
+window.onload = function () {
 
-var cube;
-var hexahedron;
-
-
-function Init()
-{
 	var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-	camera.position.z = 100;
-	
-	var renderer = new THREE.WebGLRenderer();
+	var camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
+	var renderer = new THREE.WebGLRenderer({ alpha: true });
+	var sphere;
+	var cube;
+	var cubeVertices = [];
+	var cubeCenter = [];
+	var sphereLoaded = false;
+	var cubeLoaded = false;
+
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
-	
-	var light = new THREE.PointLight(0xFFFFF);
-	light.position.set(10, 0, 25);
-	scene.add(light);
-	
-	// Cube
-	var geometry = new THREE.BoxGeometry(20, 20, 20);
-	var material = new THREE.MeshLambertMaterial({color: 0xfd59d7});
-	cube = new THREE.Mesh(geometry, material);
-	
-	scene.add(cube);
-	
-	
-	var Render = function() 
-	{
-		requestAnimationFrame(Render);
-		cube.rotation.x += 0.01;
-		cube.rotation.y += 0.01;
-		camera.updateProjectionMatrix();
-		renderer.render(scene, camera);
+
+	camera.position.z = 200;
+
+	function LoadSphere() {
+		var textureLoader = new THREE.TextureLoader();
+		var geometry = new THREE.SphereGeometry(10, 32, 32);
+		// add icosahedron
+		textureLoader.load('img/diesel.jpg', function (texture) {
+			texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+			texture.repeat.set(2, 1);
+
+			var material = new THREE.MeshLambertMaterial({ map: texture });
+			sphere = new THREE.Mesh(geometry, material);
+			sphere.position.x = 20;
+			scene.add(sphere);
+			sphereLoaded = true;
+		});
 	}
-	
+
+	function LoadSquare() {
+		var w = 10; var h = 10; var d = 10;
+		var center = [0, 0, 0];
+		var geometry = new THREE.BoxGeometry(w, h, d);
+		var material = new THREE.MeshNormalMaterial();
+		cube = new THREE.Mesh(geometry, material);
+		cube.position.x = -20;
+		center[0] += -20;
+		scene.add(cube);
+		
+		for (var i = 0; i < cube.geometry.vertices.length; i++)
+		{
+			vector = cube.geometry.vertices[i];
+			cubeVertices.push({ "x":vector.x, "y":vector.y, "z":vector.z });
+			
+			cubeCenter.push()
+		}
+		// cubeVertices = Array.from(cube.geometry.vertices);
+			
+		cubeLoaded = true;
+	}
+
+	function AddLighting() {
+
+		// so many lights
+		var light = new THREE.DirectionalLight(0xffffff, 1);
+		light.position.set(0, 1, 0);
+		scene.add(light);
+
+		light = new THREE.DirectionalLight(0xffffff, 0.5);
+		light.position.set(0, -1, 0);
+		scene.add(light);
+
+		light = new THREE.DirectionalLight(0xffffff, 1);
+		light.position.set(1, 0, 0);
+		scene.add(light);
+
+		light = new THREE.DirectionalLight(0xffffff, 0.5);
+		light.position.set(0, 0, 1);
+		scene.add(light);
+
+		light = new THREE.DirectionalLight(0xffffff, 1);
+		light.position.set(0, 0, -1);
+		scene.add(light);
+
+		light = new THREE.DirectionalLight(0xffffff, 0.5);
+		light.position.set(-1, 0, 0);
+		scene.add(light);
+	}
+
+	var prevIndex = -1;
+	function AnimateCube() {
+		var idx = -1; 
+		var vertices = cube.geometry.vertices;
+		for (var i = 0; i < vertices.length; i++) {
+			// X
+			var xdir = 1; // This is to stop it from getting to big and then immediately shrinking down, it reverses the growth
+			if (vertices[i].x > cubeVertices[i] + 10)
+				xdir *= -1;
+			if (vertices[i].x < cubeVertices[i] - 10)
+				xdir *= -1;
+			
+			var newX = Math.random() * 10 * xdir;
+			if (vertices[i].x < 0)
+				newX *= -1;
+				
+			vertices[i].x += newX;
+		}
+		
+		cube.geometry.verticesNeedUpdate = true;
+		prevIndex = idx;
+	}
+
+	function Render() {
+		if (sphereLoaded)
+			sphere.rotation.y += 0.01;
+
+		if (cubeLoaded) {
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+			
+			// AnimateCube();
+		}
+		renderer.render(scene, camera);
+		requestAnimationFrame(Render);
+	}
+
+	AddLighting();
+	LoadSphere();
+	LoadSquare();
 	Render();
 }
--- js/part4.js
+++ js/part4.js
@@ -1,116 +1,109 @@
-window.onload = Init();
+var colors = [0x05A8AA, 0xB8D5B8, 0xD7B49E, 0xDC602E, 0xBC412B, 0xF19C79, 0xCBDFBD, 0xF6F4D2, 0xD4E09B, 0xFFA8A9, 0xF786AA, 0xA14A76, 0xBC412B, 0x63A375, 0xD57A66, 0x731A33, 0xCBD2DC, 0xDBD48E, 0x5E5E5E];
+var scene, camera, renderer, geometry, mesh;
 
-function Init() {
-	var colors = [0x05A8AA, 0xB8D5B8, 0xD7B49E, 0xDC602E, 0xBC412B, 0xF19C79, 0xCBDFBD, 0xF6F4D2, 0xD4E09B, 0xFFA8A9, 0xF786AA, 0xA14A76, 0xBC412B, 0x63A375, 0xD57A66, 0x731A33, 0xCBD2DC, 0xDBD48E, 0x5E5E5E];
-	var scene, camera, renderer, geometry, mesh;
+var verticePositions = [];
 
-	var verticePositions = [];
+function initScene() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+  camera.position.z = 100;
+};
 
-	function initScene() {
-		scene = new THREE.Scene();
-		camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-		renderer = new THREE.WebGLRenderer({ alpha: true });
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		document.body.appendChild(renderer.domElement);
-		camera.position.z = 100;
-	};
+function initLighting() {
+  // so many lights
+  var light = new THREE.DirectionalLight( 0xffffff, 1 );
+  light.position.set( 0, 1, 0 );
+  scene.add( light );
 
-	function initLighting() {
-		// so many lights
-		var light = new THREE.DirectionalLight(0xffffff, 1);
-		light.position.set(0, 1, 0);
-		scene.add(light);
+  var light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  light.position.set( 0, -1, 0 );
+  scene.add( light );
 
-		var light = new THREE.DirectionalLight(0xffffff, 0.5);
-		light.position.set(0, -1, 0);
-		scene.add(light);
+  var light = new THREE.DirectionalLight( 0xffffff, 1 );
+  light.position.set( 1, 0, 0 );
+  scene.add( light );
 
-		var light = new THREE.DirectionalLight(0xffffff, 1);
-		light.position.set(1, 0, 0);
-		scene.add(light);
+  var light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  light.position.set( 0, 0, 1 );
+  scene.add( light );
 
-		var light = new THREE.DirectionalLight(0xffffff, 0.5);
-		light.position.set(0, 0, 1);
-		scene.add(light);
+  var light = new THREE.DirectionalLight( 0xffffff, 1 );
+  light.position.set( 0, 0, -1 );
+  scene.add( light );
 
-		var light = new THREE.DirectionalLight(0xffffff, 1);
-		light.position.set(0, 0, -1);
-		scene.add(light);
-
-		var light = new THREE.DirectionalLight(0xffffff, 0.5);
-		light.position.set(-1, 0, 0);
-		scene.add(light);
-	}
-
-	function initGeometry() {
-		// add icosahedron
-		geometry = new THREE.IcosahedronGeometry(20);
-		for (var i = 0; i < geometry.faces.length; i++)
-		{
-			var face = geometry.faces[i];
-			face.color.setHex(colors[i]);
-		}
-
-		mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors }));
-		scene.add(mesh);
-	}
-
-
-	function render(time) {
-		requestAnimationFrame(render);
-		renderer.render(scene, camera);
-		geometry.verticesNeedUpdate = true;
-	};
-
-	function getOriginalVerticePositions() {
-		for (var i = 0, l = geometry.vertices.length; i < l; i++)
-		{
-			verticePositions.push({ x: geometry.vertices[i].x, y: geometry.vertices[i].y });
-		}
-	}
-
-	function getNewVertices() {
-		var newVertices = [];
-		for (var i = 0, l = geometry.vertices.length; i < l; i++)
-		{
-			newVertices[i] = {
-				x: verticePositions[i].x - 5 + Math.random() * 10,
-				y: verticePositions[i].y - 5 + Math.random() * 10
-			}
-		}
-		return newVertices;
-	}
-
-	function tweenIcosahedron() {
-		var newVerticePositions = getNewVertices();
-		for (var i = 0; i < geometry.vertices.length; i++)
-		{
-			tweenVertice(i, newVerticePositions);
-		}
-	}
-
-	function tweenVertice(i, newVerticePositions) {
-		TweenLite.to(geometry.vertices[i], 1, {
-			x: newVerticePositions[i].x, y: newVerticePositions[i].y, ease: Back.easeInOut, onComplete: function () {
-				if (i === 0) tweenIcosahedron();
-			}
-		});
-	}
-
-	function resize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
-		renderer.setSize(window.innerWidth, window.innerHeight);
-	}
-
-	initScene();
-	initLighting();
-	initGeometry();
-	resize();
-	getOriginalVerticePositions();
-	render();
-	window.addEventListener("resize", resize);
-
-	tweenIcosahedron();
-
+  var light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+  light.position.set( -1, 0, 0 );
+  scene.add( light );
 }
+
+function initGeometry() {
+  // add icosahedron
+  geometry = new THREE.IcosahedronGeometry( 20 );
+  for ( var i = 0; i < geometry.faces.length; i ++ ) {
+      var face = geometry.faces[ i ];
+      face.color.setHex(colors[i]);
+  }
+
+  mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors } ));
+  scene.add( mesh );
+}
+
+
+function render(time) {
+  requestAnimationFrame( render );
+  renderer.render(scene, camera);
+  geometry.verticesNeedUpdate = true;
+};
+
+function getOriginalVerticePositions() {
+  for (var i = 0, l = geometry.vertices.length; i<l; i++) {
+    verticePositions.push({x: geometry.vertices[i].x, y: geometry.vertices[i].y});
+  } 
+}
+
+function getNewVertices() {
+  var newVertices = [];
+  for (var i = 0, l = geometry.vertices.length; i<l; i++) {
+    newVertices[i] = {
+      x: verticePositions[i].x -5 + Math.random()*10,
+      y: verticePositions[i].y -5 + Math.random()*10
+    }
+  }
+  return newVertices;
+}
+
+function tweenIcosahedron() {
+  var newVerticePositions = getNewVertices();
+  for (var i = 0; i < geometry.vertices.length; i++) {
+    tweenVertice(i, newVerticePositions);
+  }
+}
+
+function tweenVertice(i, newVerticePositions) {
+  TweenLite.to(geometry.vertices[i], 1, {x: newVerticePositions[i].x, y: newVerticePositions[i].y, ease: Back.easeInOut, onComplete: function() {
+    if (i === 0) tweenIcosahedron();
+  }});
+}
+
+function resize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+initScene();
+initLighting();
+initGeometry();
+resize();
+getOriginalVerticePositions();
+render();
+window.addEventListener("resize", resize);
+
+tweenIcosahedron();
+
+
+
+
